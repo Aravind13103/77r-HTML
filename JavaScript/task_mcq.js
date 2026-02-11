da35c13914cf27872491330e57f925135f6525ce
M. Aravind <matamaravind44@gmail.com>
2026-02-11 13:58:35 +0530

    Fixed file

---
+++ JavaScript/task_mcq.js
@@ -0,0 +1,115 @@
+// var x;
+// x = 5;
+// var x = 10;
+// console.log(x)
+
+// console.log(a);
+// var a = 10;
+// console.log(a);
+// a = 22;
+// console.log(a);
+
+// var a = 10
+// var b = "10"
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a != b);
+// console.log(a !== b);
+// console.log(a > b);
+// console.log(a === b);
+// console.log(a == b);
+// -----------------------
+
+// var age = 18
+// console.log(age>=18?"Adult":"Teen");
+
+// -----------------------
+
+// var num = 12
+// console.log(num%2==0?"Even" : "Odd");
+
+//-----------------------
+
+// var score = 97
+// console.log(score>=90?"A" : 
+//             score>=75?"B" :
+//             score>=60?"C" :
+//             score>=45?"D" : "F");
+
+// ----------------------
+
+// var temperature = 20
+// console.log(temperature > 30?"Hot" :
+//             temperature >= 20?"Warm" :
+//             temperature >= 10?"Cool" : "Cold"         
+// );
+
+//----------------------
+
+// var age_group = 6
+// console.log(age_group >= 65 ? "Senior" :
+//             age_group >= 20 ? "Adult" :
+//             age_group >= 13 ? "Teen" : "Child"
+// );
+
+//----------------------------------------------------------------------------------------------------------------------
+//  Define a function called greet that logs “Hello!” to the console. Call the function after defining it.
+
+// function greet(){
+//     console.log("Hello");
+// }
+// greet();
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Write a function named showMessage that logs “This is a message!” to the console. Call the function once after defining it.
+
+// function showMessage(){
+//     console.log("This is a message!");
+// }
+// showMessage();
+
+//----------------------------------------------------------------------------------------------------------------------
+
+//  Create a function called logInfo that prints “Logging Information…” to the console. Call the function after defining it.
+
+// function logInfo(){
+//     console.log("Logging Information...");
+
+// }
+// logInfo()
+
+//----------------------------------------------------------------------------------------------------------------------
+
+
+//  Define two functions:
+// – startGame that logs “Game Started!”
+// – endGame that logs “Game Over!”
+//  Inside the startGame function, call the endGame function.
+
+// function startGame() {
+//     console.log("Game Started!");
+//     endGame();
+// }
+
+// function endGame() {
+//     console.log("Game Over!");
+// }
+// startGame();
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Create two functions:
+// – beginTask that logs “Task Started”.
+// – finishTask that logs “Task Finished”.
+//  Inside the beginTask function, call the finishTask function.
+
+// function beginTask() {
+//     console.log("Task Started!");
+//     finishTask();
+// }
+// function finishTask() {
+//     console.log("Task Finished");
+// }
+// beginTask()
+
